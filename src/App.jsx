cb1235379f1b196bb3264fbd49a7bb27e91c845a
Samuel Viana <samuel.viana.yl@gmail.com>
2024-09-30 14:24:30 -0300

    início do port pra react (as files antigas estão na pasta pre-repatch)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path='/' Component={ Home }/>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
